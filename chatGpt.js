6850ad2e9e1c70fce37092f261c76bd6d78b3787
Darkincense <1358338656@qq.com>
2023-05-12 17:38:34 +0800

    feat: 添加chatgpt

--- chatGpt.js
+++ chatGpt.js
@@ -2,18 +2,28 @@ const { Configuration, OpenAIApi } = require("openai");
 
 (async () => {
   const configuration = new Configuration({
-    apiKey: "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",
+    // apiKey: "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",
+    apiKey: "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",
   });
   const openai = new OpenAIApi(configuration);
   
   const chapGPT = async (prompt) => {
     const response = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: prompt }],
+      // model: "text-ada-001",
+      model: "gpt-3.5-turbo",
+      // model: "gpt-3.5-turbo-0301",
+      messages: [{ role: "user", content: prompt }],
     });
-    console.log(response["data"]["choices"][0]["message"]["content"]);
+    // const response = await openai.createCompletion({
+    //   model: "text-davinci-003",
+    //   // model: "gpt-3.5-turbo",
+    //   messages: [{ role: "user", content: prompt }],
+    // });
+    console.log(response["data"]["choices"][0]["message"]);
   };
   
-  chapGPT("一个细胞核一定只有一个细胞核和一个细胞仁");
+  await chapGPT("一个细胞核一定只有一个细胞核和一个细胞仁");
+  await chapGPT("一句话概括易经");
+  await chapGPT("你的版本号是多少");
 
 })()
